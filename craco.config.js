9dd5f4e1b65c00b267b4ab957a28de367277d6d3
James Graham <james10graham@gmail.com>
2021-08-08 06:45:16 +0100

    will it blend

--- craco.config.js
+++ craco.config.js
@@ -12,9 +12,9 @@ module.exports = {
       plugin: CracoAlias,
       options: {
         source: "options",
-        baseUrl: "",
+        baseUrl: "/",
         aliases: {
-          "~": "src",
+          "~": "./src",
         },
       },
     },
