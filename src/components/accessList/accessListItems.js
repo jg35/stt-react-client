bdfb0554421bee376617f58c35246be92c3ac696
James Graham <james10graham@gmail.com>
2021-08-19 10:37:12 +0100

    acess list empty message

--- src/components/accessList/accessListItems.js
+++ src/components/accessList/accessListItems.js
@@ -6,43 +6,45 @@ export default function AccessListItems({ items, removeAccessToken }) {
       <Title size="compact" tag="h2">
         Readers
       </Title>
-      <Text>
+      <Text css="mb-6">
         When your book is private, readers will login using their email and
         login token.
       </Text>
-      <ul className="mb-8">
-        <li className="py-2 font-medium">
-          <Grid colSpan={["col-span-4"]}>
-            <span className="block">Email</span>
-            <span className="block">Login token</span>
-            <span className="block"></span>
-          </Grid>
-        </li>
-        {items
-          .filter((f) => f.type === "PRIVATE")
-          .map((item) => {
-            return (
-              <li
-                className="p-2 shadow mb-4 rounded bg-offWhite"
-                key={item.id || item.email}
-              >
-                <Grid colSpan={["col-span-4"]} css="items-center">
-                  <span className="block truncate">{item.email}</span>
-                  <span className="block truncate">{item.token}</span>
-                  <div className="flex justify-end">
-                    <Button
-                      size="compact"
-                      css="w-auto"
-                      onClick={() => removeAccessToken(item)}
-                    >
-                      Remove
-                    </Button>
-                  </div>
-                </Grid>
-              </li>
-            );
-          })}
-      </ul>
+      {items.length > 0 && (
+        <ul className="my-6">
+          <li className="py-2 font-medium">
+            <Grid colSpan={["col-span-4"]}>
+              <span className="block">Email</span>
+              <span className="block">Login token</span>
+              <span className="block"></span>
+            </Grid>
+          </li>
+          {items
+            .filter((f) => f.type === "PRIVATE")
+            .map((item) => {
+              return (
+                <li
+                  className="p-2 shadow mb-4 rounded bg-offWhite"
+                  key={item.id || item.email}
+                >
+                  <Grid colSpan={["col-span-4"]} css="items-center">
+                    <span className="block truncate">{item.email}</span>
+                    <span className="block truncate">{item.token}</span>
+                    <div className="flex justify-end">
+                      <Button
+                        size="compact"
+                        css="w-auto"
+                        onClick={() => removeAccessToken(item)}
+                      >
+                        Remove
+                      </Button>
+                    </div>
+                  </Grid>
+                </li>
+              );
+            })}
+        </ul>
+      )}
     </div>
   );
 }
