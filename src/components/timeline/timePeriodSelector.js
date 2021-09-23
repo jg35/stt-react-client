1ab8dbd6c4874acbce914212df19a79e7b957639
James Graham <james10graham@gmail.com>
2021-09-23 22:32:15 +0100

    tweak

--- src/components/timeline/timePeriodSelector.js
+++ src/components/timeline/timePeriodSelector.js
@@ -34,7 +34,7 @@ export default function TimePeriodSelector({ timelinePeriod, orphanCount }) {
   return (
     <div
       id="time-period-selector"
-      className="bg-white z-40 fixed left-0 md:left-4 bottom-0 w-full md:w-max md:bottom-4 flex items-center shadow-lg py-2 pl-4 pr-1 md:rounded-xl md:border-2 border-t border-lightGray"
+      className="bg-white z-40 fixed left-0 md:left-4 bottom-0 w-full md:w-max md:bottom-4 flex items-center shadow-lg py-2 pl-4 pr-1 md:rounded md:border-0 border-t border-lightGray"
     >
       <Text tag="span" size="compact" css="font-medium mr-2 flex">
         View <span className="hidden md:block">&nbsp;timeline&nbsp;</span> in
