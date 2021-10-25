c5693c742762ee10ca478189d9f60f02ef630935
James Graham <james10graham@gmail.com>
2021-10-25 19:51:43 +0100

    reduce shadow

--- src/components/_styled/card.js
+++ src/components/_styled/card.js
@@ -13,7 +13,7 @@ export default function Card({
   };
   const sizes = {
     compact: "p-1",
-    default: "p-4 rounded shadow md:shadow-lg md:rounded-lg",
+    default: "p-4 rounded shadow md:shadow-md md:rounded-lg",
     large: "p-8 shadow-lg rounded-md md:shadow-2xl md:rounded-lg",
   };
 
